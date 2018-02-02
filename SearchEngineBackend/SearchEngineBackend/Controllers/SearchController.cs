fa246e0099f6524c32d0f95cd001acee506097bf
mortenjull <mortenjull@hotmail.com>
2018-02-02 13:55:41 +0100

    generated project

---
+++ SearchEngineBackend/SearchEngineBackend/Controllers/SearchController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SearchEngineBackend.Controllers
+{
+    [Route("api/[controller]")]
+    public class SearchController : Controller
+    {
+        public SearchController()
+        {
+
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public async Task<IActionResult> Search()
+        {
+            
+
+            return Ok("connected");
+        }
+    }
+}
---
+++ SearchEngineBackend/SearchEngineBackend/Controllers/ValuesController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SearchEngineBackend.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [AllowAnonymous]
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [AllowAnonymous]
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [AllowAnonymous]
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [AllowAnonymous]
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [AllowAnonymous]
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
